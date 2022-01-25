9d1714dea2513c549df9e4b3acd4d4e37b1d6024
7ab901a7933419b5 <7ab901a7933419b5@protonmail.com>
2022-01-25 18:18:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let cancel = Object.fromEntries(bracketsConfig);
+  let stack = [];
+  for (const c of str.split("")) {
+    if (stack && cancel[stack[stack.length - 1]] == c) { stack.pop(); }
+    else { stack.push(c); }
+  }
+  return stack.length == 0;
 }
